3a6fa5ae694ec8dd8ce12226254c7187e467fb04
SZ2528 <83620631+SZ2528@users.noreply.github.com>
2026-01-21 17:16:49 +0800

    :construction: TODO 先吃饭去

--- nonebot_plugin_brick/handler.py
+++ nonebot_plugin_brick/handler.py
@@ -20,10 +20,31 @@ brick_alc = Alconna(
 )
 
 brick_alc.add(
+    Subcommand("烧砖", alias=["烧点砖头拍人", "烧砖"], help_text="烧点砖头拍人")
+)
+
+brick_alc.add(
     Subcommand(
-        "查看", alias=["查看砖头", "查砖"], help_text="查看自己当前砖头数和冷却状态"
+        "拍人", alias=["拍人"], help_text="拍晕（禁言）对方随机时间，有概率被反将一军"
     )
 )
+
+brick_alc.add(
+    Subcommand(
+        "随机拍人",
+        alias=["随机拍人"],
+        help_text="随机拍晕（禁言）某个群友随机时间，有概率被反将一军",
+    )
+)
+
+brick_alc.add(
+    Subcommand(
+        "查看", alias=["查看砖头", "查砖"], help_text="看看自己在这个群有多少砖头"
+    )
+)
+
+brick_alc.add(Subcommand("签到", alias=["砖头签到"], help_text="签到获取随机数量砖头"))
+
 
 brick_matcher = on_alconna(
     brick_alc,
@@ -33,6 +54,21 @@ brick_matcher = on_alconna(
     use_cmd_start=True,
     auto_send_output=True,
 )
+
+
+@brick_matcher.assign("烧砖")
+async def _(event: GroupMessageEvent, session: async_scoped_session):
+    await brick_matcher.send("[TODO] 咕咕")
+
+
+@brick_matcher.assign("拍人")
+async def _(event: GroupMessageEvent, session: async_scoped_session):
+    await brick_matcher.send("[TODO] 咕咕")
+
+
+@brick_matcher.assign("随机拍人")
+async def _(event: GroupMessageEvent, session: async_scoped_session):
+    await brick_matcher.send("[TODO] 咕咕")
 
 
 @brick_matcher.assign("查看")
@@ -50,3 +86,8 @@ async def _(event: GroupMessageEvent, session: async_scoped_session):
         await brick_matcher.send(f"你有 {brick_data.bricks}/{config.max_brick} 块砖头")
     else:
         await brick_matcher.send("你还没有砖头，使用 /砖头 烧砖 烧点吧")
+
+
+@brick_matcher.assign("签到")
+async def _(event: GroupMessageEvent, session: async_scoped_session):
+    await brick_matcher.send("[TODO] 咕咕")
